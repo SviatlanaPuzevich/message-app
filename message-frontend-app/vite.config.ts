a8ef048ca409dacacd39d9e94f115f882b928bff
puzevich <s.rudaya@sam-solutions.com>
2025-09-19 12:44:41 +0300

    build docker dev

--- message-frontend-app/vite.config.ts
+++ message-frontend-app/vite.config.ts
@@ -2,6 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-})
+  server: {
+    host: true,
+    port: 5173,
+    watch: mode === "development"
+        ? {
+          usePolling: true,
+          interval: 100,
+        }
+        : {},
+  },
+}))
